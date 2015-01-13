400b33897e88bfd6260ea0ba2dacb91931acc2f5
Jake <jakegogogo@gmail.com>
2015-01-14 00:34:52 +0800

    新增对jdbc支持6.5

--- concur/src-dbcache/dbcache/annotation/Index.java
+++ concur/src-dbcache/dbcache/annotation/Index.java
@@ -8,6 +8,8 @@ import java.lang.annotation.Target;
 /**
  * 索引属性注解
  * <br/>同hibernate的@Index
+ * <br/>使用index服务要注意:get出来的Entity对象为代理对象,实现Entity的equals方法时，
+ * <br/>避免使用诸如getClass()(可使用instanceof)或this.id == that.id(可使用this.id == this.getId())
  * @see org.hibernate.annotations.Index
  * @author Jake
  * @date 2014年9月7日下午10:50:17
