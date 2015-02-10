b029244de80d9e26a8c2b31aed8db09f8a10f65b
Jake <jakegogogo@gmail.com>
2015-02-10 23:56:30 +0800

    修改注释

--- concur/src-dbcache/dbcache/annotation/Cached.java
+++ concur/src-dbcache/dbcache/annotation/Cached.java
@@ -9,9 +9,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 实体缓存配置注解
- * 没有标注此注解的实体类则使用默认配置
+ * <h5>实体缓存配置注解</h5>
+ * <br/>没有标注此注解的实体类则使用默认配置
+ * <br/>实体持久化属性需使用基本类型或String,byte,byte[]等
+ * <br/>启用DynamicUpdate后:
+ * <br/>1,含有json格式的持久化属性对应瞬时态的Map,List等结构时需使用@ChangeFields("对应json串字段")注解标明所有会修改到json属性的方法
+ * <br/>2,外部put/add瞬时态的属性的情况需要单独提供getMapForUpdate()方法用于修改,并注解@ChangeFields("对应json串字段")
  * @see dbcache.annotation.Index
+ * @see dbcache.annotation.DynamicUpdate
+ * @see dbcache.annotation.JsonConvert
  * @author Jake
  * @date 2014年9月13日下午1:38:22
  */
