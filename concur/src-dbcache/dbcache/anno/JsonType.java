e850835a42a3bf5e643508974812b09f8a10b91d
Jake <jakegogogo@gmail.com>
2015-04-17 00:58:07 +0800

    添加EntityLoadEventListener

--- concur/src-dbcache/dbcache/anno/JsonType.java
+++ concur/src-dbcache/dbcache/anno/JsonType.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
  * json属性注解<br/>
  * <br/>可达到json字段自动转换存储
  * 对应的临时属性的定义需使用线程安全的实现类类型,如ConcurrentHashSet,ConcurrentHashMap。
+ * 需要同时实体@Transient或@Column(..)
  * @use fast-json
  * @author Jake
  */
