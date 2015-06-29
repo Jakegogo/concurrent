345e5c0feea3034c54ce43b66503c0225b5b5634
Jake <jakegogogo@gmail.com>
2015-06-29 21:29:29 +0800

    pom.xml

--- concur/src-basesource/basesource/test/TestBaseSource.java
+++ concur/src-basesource/basesource/test/TestBaseSource.java
@@ -5,7 +5,7 @@ import basesource.anno.check.Validate;
 public class TestBaseSource {
 	
 	
-	@Validate(cls = IdValidator.class, params = "1,2,3", value = {"1","2"})
+	@Validate(cls = IdValidator.class, params = "1,2,3")
 	private int id;
 	
 	static String expr() {
