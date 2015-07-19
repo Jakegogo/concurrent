1636d0d168fc10b3332252a54f032e80eb4cb602
Jake <jakegogogo@gmail.com>
2015-07-19 10:26:26 +0800

    新增PromiseActor

--- concur/src-utils/utils/typesafe/extended/MultiSafeActor.java
+++ concur/src-utils/utils/typesafe/extended/MultiSafeActor.java
@@ -86,7 +86,7 @@ public abstract class MultiSafeActor implements Runnable {
 
 	/**
 	 * 当获取所需要的关联操作后,when代表将子操作纳入本操作组成新的原子性操作
-	 * @param promiseables
+	 * @param promiseables Promiseable[] 线程安全的操作组
 	 * @return
 	 */
 	public MultiSafeActor when(Promiseable<?>... promiseables) {
